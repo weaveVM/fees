71edd924750ff764ad8f4078e664e1aa17ecd7dc
charmful0x <charmful0x@gmail.com>
2025-03-12 15:25:02 +0100

    chore: remove commented code

--- crates/fees/src/lib.rs
+++ crates/fees/src/lib.rs
@@ -22,15 +22,4 @@ impl Debug for UpdatePriceCb {
 /// This represents the value in dollars of a single WVM token.
 pub const WVM_USD_PRICE: f64 = 12.5;
 
-// mod test {
-//     use crate::ar_price_fetcher::PriceContainer;
-
-//     #[tokio::test]
-//     async fn test_price_fetch() {
-//         let price = PriceContainer::fetch_price().await.unwrap();
-//         println!("price {}", price);
-//         assert!(price > 0f64);
-//     }
-// }
-
 
